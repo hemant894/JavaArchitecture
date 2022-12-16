e8c6e19bbef5a445c17216c59a847862c447ed43
Hemant <hemant894@gmail.com>
2022-12-16 18:17:47 +0000

    JavaArchitecture

---
+++ JavaArchitecture.java
@@ -0,0 +1,10 @@
+package JavaArchitecture;
+
+public class JavaArchitecture {
+    public static void main(String[] args) { System.out.println(" JVM (Java Virtual Machine) is an abstract machine. it is a specification that provides runtime environment in which java bytecode can be excuted.");
+        System.out.println(" JRE (Java Runtime Environment) is a runtime environment which implements JVM and provides all class libraries and other files that JVM uses at runtime.");
+        System.out.println(" JDK (Java Development kit ) is the tool necessary to compile, document and package java programs. The JDK completely includes JRE.");
+
+
+    }
+}
